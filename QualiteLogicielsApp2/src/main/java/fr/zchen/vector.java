bece8564e9f30340cda3dc3aeb4ed3608cf03e91
zchen <zchen@free-mobile.fr>
2023-03-02 12:05:57 +0100

    first commit for Ex2

--- QualiteLogicielsApp2/src/main/java/fr/zchen/vector.java
+++ QualiteLogicielsApp2/src/main/java/fr/zchen/vector.java
@@ -3,7 +3,12 @@ package fr.zchen;
 import java.util.Vector;
 
 public class vector {
-
+    private Vector a;
+    private Vector b;
+    public void init (Vector _a, Vector _b){
+        a = _a;
+        b = _b;
+    }
 //    public static Vector unionSet (Vector a, Vector b){
 //
 //        return Vector Unions;
